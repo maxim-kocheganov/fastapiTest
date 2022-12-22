54d78583aecebe0415db07c31e179583e7d32ba8
maxim-kocheganov <maxim.k.elasnce@gmail.com>
2022-12-22 10:25:17 +0300

    finished task

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from time import monotonic
+import asyncio
+
+app = FastAPI()
+
+class TestResponse(BaseModel):
+    elapsed: float
+
+async def work() -> None:
+    await asyncio.sleep(3)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/test", response_model=TestResponse)
+async def handler() -> TestResponse:
+    ts1 = monotonic() 
+    await work()  
+    ts2 = monotonic()
+    return TestResponse(elapsed=ts2 - ts1)
